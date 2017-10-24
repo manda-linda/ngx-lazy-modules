2efa7c5ff4081d03793bbe4fbaae1aae7dd8fd56
adasilva <adasilva@grubhub.com>
2017-10-24 14:31:36 -0400

    Fix npm module issue

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,18 @@ export default {
     dest: './dist/bundles/ngxLazyModule.umd.js',
     format: 'umd',
     // Global namespace.
-    moduleName: 'NgxLazyModule',
+    moduleName: 'ngx.lazyModule',
     // External libraries.
     external: [
         '@angular/core',
         '@angular/common',
         'rxjs/Subject'
     ],
+    globals: {
+        '@angular/core': 'ng.core',
+        '@angular/common': 'ng.common',
+        'rxjs/Subject': 'Rx'
+    },
     onwarn: () => {
         return
     }
